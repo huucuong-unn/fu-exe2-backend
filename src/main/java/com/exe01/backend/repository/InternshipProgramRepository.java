97438dff4541ceb946b1a23903ce524409120842
nguyenthanh311003 <nguyenthanh311003@gmail.com>
2024-09-10 21:11:56 +0700

    implement top 3 internship program api

--- src/main/java/com/exe01/backend/repository/InternshipProgramRepository.java
+++ src/main/java/com/exe01/backend/repository/InternshipProgramRepository.java
@@ -11,11 +11,12 @@ import java.util.UUID;
 @Repository
 public interface InternshipProgramRepository extends JpaRepository<InternshipProgram, UUID> {
     @Query("SELECT ip FROM InternshipProgram ip " +
-            "JOIN Application a ON ip.id = a.id " +
+            "JOIN Application a ON ip.id = a.internshipProgram.id " +
             "WHERE ip.status = 'OPEN' " +
             "AND FUNCTION('MONTH', a.createdDate) = FUNCTION('MONTH', CURRENT_DATE) " +
             "AND FUNCTION('YEAR', a.createdDate) = FUNCTION('YEAR', CURRENT_DATE) " +
             "GROUP BY ip.id " +
-            "ORDER BY COUNT(a.id) DESC")
+            "ORDER BY COUNT(a.id) DESC " +
+            "LIMIT 3")
     List<InternshipProgram> findTop3InternshipProgramsByApplications();
 }
