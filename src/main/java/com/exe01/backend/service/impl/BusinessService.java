c05d17a661786d0d131e6fd82e3cef95c7453a35
nguyenthanh311003 <nguyenthanh311003@gmail.com>
2024-09-12 22:08:28 +0700

    implement get internship program search sort api and fix warning

--- src/main/java/com/exe01/backend/service/impl/BusinessService.java
+++ src/main/java/com/exe01/backend/service/impl/BusinessService.java
@@ -21,7 +21,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
 import java.util.UUID;
 
